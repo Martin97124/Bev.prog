6c47bb7f850d45e83de5dcd5fd4fdaa17ae28ea8
Martin97124 <43417765+Martin97124@users.noreply.github.com>
2018-09-26 23:27:17 +0200

    Add files via upload

---
+++ Labor2különbséggel.cpp
@@ -0,0 +1,14 @@
+#include "std_lib_facilities.h"
+
+int main ()
+{
+int a = 7;
+int b = 1;
+cout<<"Jelenleg : a:"<< a << " " "b:" << b << endl;
+
+b = b + a;
+a = b - a;
+b = b - a;
+
+cout<<"Utánna : a:"<< a << " " "b:" << b << endl;
+}
